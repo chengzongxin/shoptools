a9388b6eee9edc10144033a8b2fd79cb00d8b75b
chengzongxin <8876142@qq.com>
2025-08-03 15:06:28 +0800

    Update version to V1.1.2 and add secondary select status list in ConfirmUploadCrawler

--- TEMUTools/src/main.py
+++ TEMUTools/src/main.py
@@ -277,7 +277,7 @@ class TEMUToolsApp:
     
     def __init__(self, root):
         self.root = root
-        self.root.title("TEMU工具集 V1.1.1")
+        self.root.title("TEMU工具集 V1.1.2")
         self.logger = Logger()
         
         # 创建标签页
