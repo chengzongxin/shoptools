06eb7701796a33d3911dc2f8460ffdb69ba137ae
chengzongxin <8876142@qq.com>
2025-10-12 23:34:23 +0800

    1.5.4 默认配置

--- TEMUTools/src/main.py
+++ TEMUTools/src/main.py
@@ -279,7 +279,7 @@ class TEMUToolsApp:
     
     def __init__(self, root):
         self.root = root
-        self.root.title("TEMU工具集 V1.5.3")
+        self.root.title("TEMU工具集 V1.5.4")
         self.logger = Logger()
         
         # 创建标签页
